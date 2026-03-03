76e8f93624716ca873a1b2ac7c2eaa7e2c3ec2d0
root <root@webmail.dptcloud.vn>
2026-03-03 01:15:37 +0000

    Add nodejs builds to GitHub Actions

--- build-all.sh
+++ build-all.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
 REGISTRY="103.176.24.13:5000"
 
-# ===== Python =====
-for v in 3.7 3.8 3.9 3.10 3.11 3.12 3.13; do
-    echo "====== Building python$v ======"
-    cd /root/python/$v && docker build --compress -t ${REGISTRY}/python$v:latest . && docker push ${REGISTRY}/python$v:latest
+# ===== Pterodactyl Node.js =====
+for v in 14 16 17 18 19 20 21 22 23 24 25; do
+    echo "====== Building pterodactyl-nodejs$v ======"
+    cd /root/pterodactyl-nodejs$v && docker build --compress -t ${REGISTRY}/pterodactyl-nodejs$v:latest . && docker push ${REGISTRY}/pterodactyl-nodejs$v:latest
 done
-
-# ===== Pterodactyl Java =====
-for v in 8 11 16 17 18 19 21 22 23 24 25; do
-    echo "====== Building pterodactyl-java$v ======"
-    cd /root/pterodactyl-java$v && docker build --compress -t ${REGISTRY}/pterodactyl-java$v:latest . && docker push ${REGISTRY}/pterodactyl-java$v:latest
-done
-
-# ===== VPSBumboo =====
-for v in 8 11 16 17 18 19 21 22 23 25; do
-    echo "====== Building pterodactyl-java$v-vpsbumboo ======"
-    cd /root/vpsbumboo/pterodactyl-java$v && docker build --compress -t ${REGISTRY}/pterodactyl-java$v-vpsbumboo:latest . && docker push ${REGISTRY}/pterodactyl-java$v-vpsbumboo:latest
-done
-
-# ===== Node.js =====
-for v in 12 14 16 17 18 19 20 21 22 23 24; do
-    echo "====== Building nodejs$v ======"
-    cd /root/nodejs/$v && docker build --compress -t ${REGISTRY}/nodejs$v:latest . && docker push ${REGISTRY}/nodejs$v:latest
-done
-
 echo "====== ALL BUILDS COMPLETE ======"
